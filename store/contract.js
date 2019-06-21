d89279febaa69c30d5bfda0eb91dc158b285a78d
Shubhendu Shekhar <shubh.shekhar1991@gmail.com>
2019-06-21 18:49:20 +0200

    add ipfs and contract related store

---
+++ store/contract.js
@@ -0,0 +1,8 @@
+/**
+ * All the contract related operations
+ */
+import Web3 from 'web3'
+// eslint-disable-next-line no-unused-vars
+import contract from 'truffle-contract'
+// eslint-disable-next-line no-unused-vars
+const provider = new Web3.providers.HttpProvider('http://localhost:7545')
---
+++ store/ipfs.js
@@ -0,0 +1,4 @@
+import ipfsClient from 'ipfs-http-client'
+
+// eslint-disable-next-line no-unused-vars
+const ipfs = ipfsClient('172.20.0.101', '5001', { protocol: 'http' })
