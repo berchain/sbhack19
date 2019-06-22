f795aaa18d81aa96db058a02b8c01c039e3bc691
Emil Sommerfeldt <emil@jungfeldt.de>
2019-06-22 21:26:35 +0200

    right blue

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -7,7 +7,7 @@
       <input type="password" placeholder="Password" class="p-3 w-full " />
     </div>
     <div class="m-5">
-      <button class="p-3 w-full" style="background-color: rgb(64, 90, 14)">
+      <button class="p-3 w-full" style="background-color: rgb(64, 90, 148)">
         Facebook login
       </button>
     </div>
