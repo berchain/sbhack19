9b97a1ce1cbcb44ad57c64fab052acf3d5883639
Shubhendu Shekhar <shubh.shekhar1991@gmail.com>
2019-06-22 15:36:04 +0200

    fix contract for supply chain. string doesn't works still.

--- contracts/SupplyChain.sol
+++ contracts/SupplyChain.sol
@@ -14,51 +14,51 @@ contract SupplyChain {
         Stage stage;
         string ipfsInfo;
     }
-    mapping (string => Product) public productMap;
+    mapping (uint => Product) public productMap;
 
-    event ProductUpdated(string, Stage, string);
+    event ProductUpdated(uint, Stage, string);
 
     constructor() public {
         owner = msg.sender;
     }
 
-    function addProduct(string memory ipfs, string memory id, address certifier) public {
+    function addProduct(string memory ipfs, uint id, address certifier) public {
         Product memory product = Product(msg.sender, certifier, address(0), address(0), address(0), Stage.ProductCreated, ipfs);
         productMap[id] = product;
         emit ProductUpdated(id, Stage.ProductCreated, ipfs);
     }
 
-    function markPackaged(string memory ipfs, string memory id) public {
+    function markPackaged(string memory ipfs, uint id) public {
         productMap[id].ipfsInfo = ipfs;
         productMap[id].certifier = msg.sender;
         emit ProductUpdated(id, Stage.ProductPackaged, ipfs);
     }
 
-    function sendToProcessor(string memory ipfs, string memory id, address processor) public {
+    function sendToProcessor(string memory ipfs, uint id, address processor) public {
         productMap[id].ipfsInfo = ipfs;
         productMap[id].processor = processor;
         emit ProductUpdated(id, Stage.SentToProcessor, ipfs);
     }
 
-    function sendToDistributor(string memory ipfs, string memory id, address distributor) public {
+    function sendToDistributor(string memory ipfs, uint id, address distributor) public {
         productMap[id].ipfsInfo = ipfs;
         productMap[id].distributor = distributor;
         emit ProductUpdated(id, Stage.SentToDistributor, ipfs);
     }
 
-    function sendToRetailer(string memory ipfs, string memory id, address retailer) public {
+    function sendToRetailer(string memory ipfs, uint id, address retailer) public {
         productMap[id].ipfsInfo = ipfs;
         productMap[id].retailer = retailer;
         emit ProductUpdated(id, Stage.SentToRetailer, ipfs);
     }
 
-    function markSold(string memory ipfs, string memory id) public {
+    function markSold(string memory ipfs, uint id) public {
         assert(msg.sender == productMap[id].retailer);
         productMap[id].ipfsInfo = ipfs;
         emit ProductUpdated(id, Stage.Sold, ipfs);
     }
 
-    function getProductInfo(string memory id) public view returns(string memory) {
+    function getProductInfo(uint id) public view returns(string memory) {
         return productMap[id].ipfsInfo;
     }
 
