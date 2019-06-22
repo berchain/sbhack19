8a06a258ab3eaba198202f8c5cb91d8d9861cdb4
Shubhendu Shekhar <shubh.shekhar1991@gmail.com>
2019-06-23 00:10:00 +0200

    fix contract logic

--- contracts/donation.sol
+++ contracts/donation.sol
@@ -2,25 +2,21 @@ pragma solidity >=0.4.0 <0.7.0;
 
 import 'openzeppelin-solidity/contracts/token/ERC20/ERC20.sol';
 
-contract LibraToken is ERC20 {
-
-  string public constant name = "LibraToken";
-  string public constant symbol = "LIT";
-  uint8 public constant decimals = 18;
-
-  uint256 public constant INITIAL_SUPPLY = 10000 * (10 ** uint256(decimals));
-
-  constructor() public {
-    _mint(msg.sender, INITIAL_SUPPLY);
-  }
-}
-
 contract Donation is ERC20 {
+
+    string public constant name = "LibraToken";
+    string public constant symbol = "LIT";
+    uint8 public constant decimals = 18;
+    uint public constant TokenPriceWei = 2000;
+
+    uint256 public constant INITIAL_SUPPLY = 10000 * (10 ** uint256(decimals));
     event donated(address indexed _from, address indexed _to, uint256 amount);
 
     function buy() public payable {
       //the value of Libra is equal to ethers.
-      _mint(msg.sender, msg.value);
+      uint decimals = 10**18;
+      uint numberoftokens = (msg.value*decimals)/TokenPriceWei;
+      _mint(msg.sender, numberoftokens);
     }
 
     function donate(address payable to, uint256 amount) public{
