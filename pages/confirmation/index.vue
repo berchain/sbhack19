ed19764bd57ab8a70850fe5dc33d6df060fd0853
Emil Sommerfeldt <emil@jungfeldt.de>
2019-06-23 00:56:34 +0200

    add styling to confirmation, update backbutton

--- pages/confirmation/index.vue
+++ pages/confirmation/index.vue
@@ -1,40 +1,40 @@
 <template>
   <app-view>
     <figure>
-      <h1>Thank you!</h1>
+      <h1 class="text-white text-6xl font-semibold">Thank you!</h1>
     </figure>
     <article>
-      <h2>Track your donations</h2>
-      <section>
+      <h2 class="text-2xl font-semibold">Track your donations</h2>
+      <section class="border-white border-t-4 mt-5">
         <ul>
-          <li>
-            <div>
-              <figure class="w-24 h-24 rounded-full overflow-hidden">
-                <img
-                  src="../../assets/images/banana.jpeg"
-                  alt=""
-                  class="w-4/5"
-                />
+          <li class="bg-yellow-500 py-10">
+            <div class="flex">
+              <figure class="w-24 rounded-full overflow-hidden block">
+                <img src="../../assets/images/banana.jpeg" alt="" />
               </figure>
               <article class="ml-5">
                 <h3 class="font-semibold">Fair-trade</h3>
                 <p>
-                  See transaction on Blockchain
-                  <a href="#querystring">{{ hash }}</a>
+                  See transaction in Blockchain
+                  <a href="#querystring">Explorer {{ hash }}</a>
                 </p>
               </article>
             </div>
           </li>
         </ul>
       </section>
+      <footer>
+        <app-button-back cta="Go to start" url="/" />
+      </footer>
     </article>
   </app-view>
 </template>
 
 <script>
 import AppView from '../../components/AppView'
+import AppButtonBack from '../journey/AppButtonBack'
 export default {
-  components: { AppView },
+  components: { AppButtonBack, AppView },
   data() {
     return {
       hash: ''
--- pages/journey/AppButtonBack.vue
+++ pages/journey/AppButtonBack.vue
@@ -1,10 +1,24 @@
 <template>
-  <button class="py-3 text-2xl font-semibold w-full" @click="$router.back()">
+  <button class="py-3 text-2xl font-semibold w-full" @click="goTo">
     Go back
   </button>
 </template>
 <script>
 export default {
-  name: 'AppButtonBack'
+  name: 'AppButtonBack',
+  props: {
+    url: {
+      type: String,
+      default: ''
+    }
+  },
+  methods: {
+    goTo() {
+      if (!this.url) {
+        this.$router.back()
+      }
+      this.$router.push(this.url)
+    }
+  }
 }
 </script>
