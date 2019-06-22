9d45eaf9a0090e8af74b0a539e4e84eeddd1d655
Shresth Agrawal <shresthagrawal.31@gmail.com>
2019-06-22 14:39:26 +0530

    solidity: Add basic donation contract

---
+++ contracts/donation.sol
@@ -0,0 +1,14 @@
+pragma solidity >=0.4.0 <0.7.0;
+
+contract donation{
+    event donated(address indexed _from, address indexed _to, uint256 amount);
+    
+    function donate(address payable _to) public payable{
+        transfer(msg.sender, _to, msg.value);
+    }
+    
+    function transfer(address _from, address payable _to, uint256 _amount) internal{
+        _to.transfer(_amount);
+        emit donated(_from, _to, _amount);
+    }
+}
